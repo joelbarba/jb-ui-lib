1abd0bf20e743e20e0317940f8241dc35a017d32
joel.barba <joel.barba.vidal@gmail.com>
2021-07-04 19:00:52 +0200

    Add prototype JbNumber.pad

--- projects/jb-ui-lib/src/lib/jb-prototypes/number.prototype.ts
+++ projects/jb-ui-lib/src/lib/jb-prototypes/number.prototype.ts
@@ -15,4 +15,23 @@ JbNumber.isIn = function(list: Array<number>): boolean {
   return (list.indexOf(this.valueOf()) >= 0);
 };
 
+/**
+ * @memberOf Number
+ * @param leftPad - Size of the output string counting the length of the original string + padding left
+ * @param placeholder - Value to fulfill the padding (0 by default)
+ * @param rightPad - Size of the output string counting the length of the original string + padding right
+ * @description It coverts the number into a string and applies the pad
+ *              Shortcut for .padStart() / .padEnd(). Ex: myValue.pad(4) -> 0003
+ */
+JbNumber.pad = function(leftPad: number, placeholder = '0', rightPad?: number): string {
+  const value = this + '';
+  const originalLength = value.length;
+  let strValue = value.padStart(leftPad, placeholder);
+  if (!!rightPad) {
+    strValue = strValue.padEnd(leftPad + rightPad - originalLength, placeholder);
+  }
+  return strValue;
+};
+
+
 export default JbNumber;
--- update_doc.sh
+++ update_doc.sh
@@ -4,8 +4,8 @@ ng build jb-ui-lib-sandbox
 
 ver=$(node -p -e "require('./projects/jb-ui-lib/package.json').version")
 echo "Deploying document reference. version = $ver"
-cp -r dist/jb-ui-lib-sandbox/* ../doc-jb-ui-lib
-cd ../doc-jb-ui-lib
+cp -r dist/jb-ui-lib-sandbox/* ../jb-ui-lib-doc
+cd ../jb-ui-lib-doc
 git add -A
 git commit -m "jb-ui-lib: $ver"
 git push
